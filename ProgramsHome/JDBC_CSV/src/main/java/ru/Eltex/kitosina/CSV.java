452bcd888a3ba3d3a19480373083304031b2ebca
kitosina <kitosina@mail.ru>
2019-07-29 23:21:41 +0700

    Init

--- ProgramsHome/JDBC_CSV/src/main/java/ru/Eltex/kitosina/CSV.java
+++ ProgramsHome/JDBC_CSV/src/main/java/ru/Eltex/kitosina/CSV.java
@@ -1,6 +1,9 @@
 package ru.Eltex.kitosina;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface CSV {
-    void fromCSV();
-    void toDateBase();
+    void fromCSV() throws IOException;
+    void toDateBase() throws SQLException;
 }
